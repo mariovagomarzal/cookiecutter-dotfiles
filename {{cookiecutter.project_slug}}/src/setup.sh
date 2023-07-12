29870fedd5abe3b33e33ea6128f976c4a66b753c
Mario Vago Marzal <mariovagomarzal@gmail.com>
2023-07-12 12:36:35 +0200

    Move back the OS recognition function to 'setup.sh'
    The function was removed from 'utils.sh' script in the last commit but it wasn't added to the 'setup.sh' script back

--- {{cookiecutter.project_slug}}/src/setup.sh
+++ {{cookiecutter.project_slug}}/src/setup.sh
@@ -22,6 +22,35 @@ export BOOTSTRAP_ORDER=(
 )
 
 
+# ┌────────────────┐
+# │ OS recognition │
+# └────────────────┘
+
+# - - - - - - - - - - - - - - - - - - - -
+# Get the OS name.
+# Arguments:
+#   None.
+# Returns:
+#   The OS name. Possible outputs are:
+#     - linux
+#     - macos
+#     - unknown
+# - - - - - - - - - - - - - - - - - - - -
+get_os_name() {
+    local os_name=""
+    
+    if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+        os_name="linux"
+    elif [[ "$OSTYPE" == "darwin"* ]]; then
+        os_name="macos"
+    else
+        os_name="unknown"
+    fi
+    
+    printf $os_name
+}
+
+
 # ┌─────────────────────┐
 # │ Sudo authentication │
 # └─────────────────────┘
