86e582e08e7ee0b413c95adbdf86833a7f95f90d
Mario Vago Marzal <mariovagomarzal@gmail.com>
2023-07-17 23:15:32 +0200

    Add the LICENSE file to the template
    Improve the tests to check that the LICENSE and other files are properly generated
    
    Note that the license file of the template does not affect the license of the project itself

--- README.md
+++ README.md
@@ -55,3 +55,5 @@ for more information on how to do that.
 ## License
 
 This project is licensed by [Mario Vago Marzal](https://github.com/mariovagomarzal) under the terms of the [MIT License](/LICENSE).
+
+_Note: The [LICENSE](/{{cookiecutter.project_slug}}/LICENSE) file in the `{{cookiecutter.project_slug}}` directory is part of the template and does not affect the license of the project itself._
--- tests/test_cookiecutter.py
+++ tests/test_cookiecutter.py
@@ -1,10 +1,72 @@
 """Test if the cookiecutter template renders correctly."""
+import pytest
 
-def test_cookiecutter(cookies):
+
+@pytest.fixture
+def extra_context():
+    return {
+        "project_slug": "dotfiles-test",
+        "license": "Apache-2.0",
+        "macos_support": "yes",
+        "linux_support": "no",
+    }
+
+
+# Bake without extra context
+def test_cookiecutter_no_context(cookies):
     result = cookies.bake()
+    path = result.project_path
 
+    # No errors.
     assert result.exit_code == 0
     assert result.exception is None
 
-    assert result.project_path.name == "dotfiles"
-    assert result.project_path.is_dir()
+    # Check if the project was created correctly.
+    assert path.is_dir()
+    assert path.name == "dotfiles"
+
+    # Check some contents.
+    assert (path / "README.md").is_file()
+    assert "Your Name" in (path / "README.md").read_text()
+
+    assert (path / "LICENSE").is_file()
+    assert "MIT" in (path / "LICENSE").read_text()
+
+    assert (path / "common").is_dir()
+    for system in ["macos", "linux"]:
+        assert (path / system).is_dir()
+        assert (path / f"install_order_{system}.txt").is_file()
+        assert (path / f"bootstrap_order_{system}.txt").is_file()
+
+# Bake with extra context
+def test_cookiecutter_context(cookies, extra_context):
+    result = cookies.bake(extra_context=extra_context)
+    path = result.project_path
+
+    # No errors.
+    assert result.exit_code == 0
+    assert result.exception is None
+
+    # Check if the project was created correctly.
+    assert path.is_dir()
+    assert path.name == "dotfiles-test"
+
+    # Check some contents.
+    assert (path / "README.md").is_file()
+    assert "Your Name" in (path / "README.md").read_text()
+
+    assert (path / "LICENSE").is_file()
+    assert "Apache" in (path / "LICENSE").read_text()
+
+    assert (path / "common").is_dir()
+
+    # Check if the contents exist for macos.
+    assert (path / "macos").is_dir()
+    assert (path / "install_order_macos.txt").is_file()
+    assert (path / "bootstrap_order_macos.txt").is_file()
+
+    # Check if the contents exist for linux
+    # (should not exist because linux_support is set to "no").
+    assert not (path / "linux").is_dir()
+    assert not (path / "install_order_linux.txt").is_file()
+    assert not (path / "bootstrap_order_linux.txt").is_file()
