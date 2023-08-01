6f71d9c04ad1aab2189dd2a65697a0b9ccc47259
Mario Vago Marzal <mariovagomarzal@gmail.com>
2023-08-01 13:16:02 +0200

    Add a docs test and the 'CI/CD tests' action

--- noxfile.py
+++ noxfile.py
@@ -1,6 +1,6 @@
 import nox
 
-@nox.session
+@nox.session(python=["3.7", "3.8", "3.9", "3.10", "3.11"])
 def tests(session):
     session.install("pytest", "pytest-cookies")
     session.run("pytest")
@@ -9,3 +9,8 @@ def tests(session):
 def lint(session):
     session.install("ruff")
     session.run("ruff", "check", ".")
+
+@nox.session
+def docs(session):
+    session.install("mkdocs-material")
+    session.run("mkdocs", "build", "--clean")
